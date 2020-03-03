cab3a29999dcf9259a22c741c704e195783fc564
wangqi <2900303808@qq.com>
2020-03-03 13:17:07 +0800

    直播间

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -4,72 +4,105 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script type="text/javascript" charset="utf-8" src="https://g.alicdn.com/de/prismplayer/2.8.7/aliplayer-min.js"></script>
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.js"></script>
+    <link rel="stylesheet" href="https://g.alicdn.com/de/prismplayer/2.8.7/skins/default/aliplayer-min.css" />
+    <script type="text/javascript" charset="utf-8" src="https://g.alicdn.com/de/prismplayer/2.8.7/aliplayer-min.js"></script>
     <title>直播间</title>
 </head>
 <body>
-        <div style="width: 70%;height: 400px;border: 1px solid black;float: left;" class="prism-player" id="player-con"></div>
-        <div style="width: 29%;height: 800px;float: right;">
-                <div style="width: 99%;height: 600px;border: 1px solid black;">
-                    <input type="button" value="大家说" style="width:49%;height:8%" class="speak">
-                    <input type="button" value="粉丝贡献榜" style="width:49%;height:8%" class="fensi">
-                </div>
-                <input type="text" id="message" style="width:99%;margin-top:20px">
-                <input type="button" value="发送" id="btn" style="width:33%;margin-left:33%;margin-top:20px">
-        </div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
-        <div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div style="width: 70%;height: 500px;border: 1px solid black;float: left;" class="prism-player" id="player-con"></div>
+<div style="width: 29%;float:right;">
+    <input type="button" value="☺大家说☺" style="width:49%;height:40px">
+    <input type="button" value="☀粉丝贡献榜☀" style="width:49%;height:40px">
+    <div style="width: 99%; height: 600px; border: 1px solid black;overflow: auto" id="list"></div>
+    <input type="text" id="message">
+    <input type="button" value="发送" id='btn'>
+    <img src="./bq.jpg" alt="添加表情" style="width: 30px; height: 30px; margin-top: 15px" id="bq">
+    {{--<div id="bqlist" style="width: 70%; height: auto"></div>--}}
+    <div id="bqlist" style="width: 400px;height: auto;"></div>
+</div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
+<div  style="width: 100px;height: 100px;border: 1px solid black;float: right; margin:15px 15px 15px 15px" ></div>
 </body>
 <script>
-    // var player = new Aliplayer({
-    //     "id": "player-con",
-    //     "source": "rtmp://youke.bianaoao.top/myfiestvideo/video?auth_key=1583072373-0-0-a6c51bdee40c8147e12cec60b84e9a6e",
-    //     "width": "100%",
-    //     "height": "500px",
-    //     "autoplay": true,
-    //     "isLive": false,
-    //     "rePlay": false,
-    //     "playsinline": true,
-    //     "preload": true,
-    //     "controlBarVisibility": "hover",
-    //     "useH5Prism": true
-    // }, function(player) {
-    //     console.log("The player is created");
-    // });
-    //用户登录之后 获取用户名
+    $(document).on('click','.dajia',function(){
+        var _this=$(this);
+        var div = _this.parent().parent().html();
+        console.log(div);
+    });
+    var player = new Aliplayer({
+        "id": "player-con",
+        "source": "rtmp://youke.bianaoao.top/filevido/vido?auth_key=1583117148-0-0-4efcff3b7648a2dac22eb6b65654ecc4",
+        "width": "100%",
+        "height": "500px",
+        "autoplay": true,
+        "isLive": false,
+        "rePlay": false,
+        "playsinline": true,
+        "preload": true,
+        "controlBarVisibility": "hover",
+        "useH5Prism": true
+    }, function(player) {
+        console.log("The player is created");
+    });
     var username = prompt('请输入用户名');
-    var ws = new WebSocket("ws://116.62.160.207:9502");
-    // console.log(ws);
-    ws.onopen = function(){
+    var ws = new WebSocket("ws://182.92.161.74:9502");
+    ws.onopen = function() {
         var message = '{"type":"login","con":"' + username + '"}';
         ws.send(message);
     }
-
-    ws.onmessage = function(res){
+    ws.onmessage = function(res) {
         var data = JSON.parse(res.data);
         if (data.is_me == 1 && data.type == 'login') {
-            var con = "<p style='text-align:center'>尊敬的用户：" + data.username + "欢迎您的到来</p>";
+            var con = "<p style='text-align:center'>♠尊敬的用户：" + data.username + "欢迎您的到来♠</p>";
         } else if (data.is_me == 0 && data.type == 'login') {
-            var con = "<p style='text-align:center'>系统提示：" + data.username + "上线</p>";
+            var con = "<p style='text-align:center'>♠系统提示：" + data.username + "上线♠</p>";
         } else if (data.is_me == 1 && data.type == 'message') {
-            var con = "<div style='width: 450px; height: 100px;' align='right'><p>来自您的消息</p><p></p><p>" + data.message + "</p></div>"
+            var con = "<div style='width: 99%; height: 100px;' align='right'><p>♥我♥</p><p></p><p>" + data.message + "</p></div>"
         } else if (data.is_me == 0 && data.type == 'message') {
-            var con = "<div style='width: 300px; height: 100px;' alig='left'><p>来自" + data.username + "的消息</p><p></p><p>" + data.message + "</p></div>"
+            var con = "<div style='width: 300px; height: 100px;' alig='left'><p>♠来自用户" + data.username + "的消息♠</p><p></p><p>" + data.message + "</p></div>"
         } else if (data.is_me == 0 && data.type == 'loginout') {
-            var con = "<p style='text-align:center'>系统提示：" + data.username + "离开了聊天室</p>";
+            var con = "<p style='text-align:center'>☹系统提示：" + data.username + "离开了聊天室☹</p>";
         }
-        $("#speak").text(res.data);
+        $("#list").append(con);
     }
     $(document).on('click', '#btn', function() {
         var con = $('#message').val();
         var message = '{"type":"message","con":"' + con + '"}';
         ws.send(message);
     });
+    $(document).on("click","#bq",function(){
+        $.ajax({
+            url:'./bq.php',
+            dataType:'json',
+            success:function(res){
+                // console.log(res);
+                //    如果返回值是纯黑色字体，是字符串
+                //    var data = eval("("+res+")");     使用这个函数进行转换
+                var img = '';
+                for(var i in res){
+                    // console.log(res[i]);
+                    img +="<img class='bqimg' src='./bq/"+res[i]+"' style='width: 50px;height: 50px;'>";
+                }
+
+                // console.log(img);
+                $("#bqlist").html(img);
+            }
+
+        })
+    })
+
+    $(document).on('click',".bqimg",function(){
+        var src = $(this).attr("src");
+        var con = "<img src='"+src+"'>";
+        // console.log(con);
+        var message = '{"type":"message","con":"'+con+'"}';
+        ws.send(message);
+    })
 </script>
 </html>
--- server.php
+++ server.php
@@ -1,111 +1,21 @@
 <?php
-new Swoole\WebSocket\Server;
 
-//创建websocket服务器对象，监听0.0.0.0:9502端口
-$ws = new Server ("0.0.0.0", 9502);
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-///监听WebSocket连接打开事件
-$ws->on('open', function ($ws, $request) {
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-});
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
 
-//监听WebSocket消息事件
-$ws->on('message', function ($ws, $frame) {
-    // var_dump($frame);
-    $info = json_decode($frame->data,true);
-    if($info['type']=='login'){
-        $redis=new Swoole\Coroutine\Redis();
-        $key="online_list";
-        $redis->connect('127.0.0.1',6379);
-        $list=$redis->get($key);
-        $userlist=json_decode($list,true);
-        if(empty($userlist)){
-            $userlist=[];
-        }
-        $userlist[]=[
-            'client_id'=>$frame->fd,
-            'username'=>$info['con']
-        ];
-        $str=json_encode($userlist,JSON_UNESCAPED_UNICODE);
-        $redis->set($key,$str);
-        $message=[
-            'type'=>'login',
-            'is_me'=>1,
-            'username'=>$info['con']
-        ];
-        $res=json_encode($message,JSON_UNESCAPED_UNICODE);
-        $ws->push($frame->fd, $res);
-        foreach($userlist as $key=>$val){
-            if($frame->fd != $val['client_id']){
-                $message=[
-                    'type'=>'login',
-                    'is_me'=>0,
-                    'username'=>$info['con']
-                ];
-                $res=json_encode($message,JSON_UNESCAPED_UNICODE);
-                $ws->push($val['client_id'], $res);
-            }
-        }
-    }elseif($info['type']=='message'){
-        $redis = new Swoole\Coroutine\Redis();
-        $key = "online_list";
-        $redis->connect('127.0.0.1',6379);
-        $list=$redis->get($key);
-        $userlist = json_decode($list,true);
-        foreach ($userlist as $k=>$v){
-            if($v['client_id']==$frame->fd){
-                $name = $v['username'];
-            }
-
-        }
-        foreach ($userlist as $key=>$value){
-            if($value['client_id']== $frame->fd){
-                $message=[
-                    'type'=>'message',
-                    'is_me'=>1,
-                    'username'=>$name,
-                    'message'=>$info['con']
-                ];
-            }else{
-                $message=[
-                    'type'=>'message',
-                    'is_me'=>0,
-                    'username'=>$name,
-                    'message'=>$info['con']
-                ];
-            }
-            $res = json_encode($message,JSON_UNESCAPED_UNICODE);
-            $ws->push($value['client_id'], $res);
-        }
-    }
-
-});
-
-//监听WebSocket连接关闭事件
-$ws->on('close', function ($ws, $fd) {
-    $redis=new Swoole\Coroutine\Redis();
-    $key = "online_list";
-    $redis->connect('127.0.0.1',6379);
-    $list =$redis->get($key);
-    $userlist = json_decode($list,true);
-    foreach($userlist as $key=>$value){
-        if($value['client_id']== $fd){
-            unset($userlist[$key]);
-            $name = $value['username'];
-        }
-    }
-    $str = json_encode($userlist);
-    $redis->set($key,$str);
-    foreach ($userlist as $k=>$v){
-        $message=[
-            'type'=>'loginout',
-            'is_me'=>0,
-            'username'=>$name,
-
-        ];
-        $res = json_encode($message,JSON_UNESCAPED_UNICODE);
-        $ws->push($v['client_id'], $res);
-    }
-});
-
-$ws->start();
+require_once __DIR__.'/public/index.php';
